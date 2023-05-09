a646a050a69ab44af324f09282a1376b96d77e2c
prabhat1510 <prabhatc@gmail.com>
2023-05-09 20:32:09 +0530

    App with route and axios

--- appwithrouteslatest/src/sendDetailsToServer.js
+++ appwithrouteslatest/src/sendDetailsToServer.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
+//import url from 'url';
+//const url = require("url");
 const sendDetailsToServer=(props)=>{
     let payload= {
         "email": props.email.toString(),
@@ -11,13 +13,15 @@ const sendDetailsToServer=(props)=>{
         'Access-Control-Allow-Origin': '*'
         }
     };
+
+   // const params = new url.URLSearchParams(payload.email); 
     /**
     axios.post('http://localhost:8080/login',payload,customConfig).then(function(response){
             console.log(response.status);
             console.log(response);
     }); */
 
-    axios.get('http://localhost:8080/logindetails/',payload,customConfig).then(function (response){
+    axios.get(`http://localhost:8080/logindetails?email=`+props.email.toString()).then(function (response){
         console.log(response.status);
         console.log(response);
     }
